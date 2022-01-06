eaf8e65e0ec6e7c4e5391fb26912f790c171720d
barsheshet <guybarsheshet1@gmail.com>
2022-01-06 09:23:18 +0200

    partial 06-01

---
+++ 2022-01-06-react/01 - Northwind/src/Components/LayoutArea/Header/Header.tsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+function Header(): JSX.Element {
+  return (
+    <div className="Header">
+      <h1>Northwind Website</h1>
+    </div>
+  );
+}
+
+export default Header;
---
+++ 2022-01-06-react/01 - Northwind/src/Components/LayoutArea/Menu/Menu.tsx
@@ -0,0 +1,16 @@
+import "./Menu.css";
+
+function Menu(): JSX.Element {
+    return (
+        <div className="Menu">
+			<nav>
+                <a href="#">Home</a>
+                <a href="#">Products</a>
+                <a href="#">About</a>
+                <a href="#">Contact Us</a>
+            </nav>
+        </div>
+    );
+}
+
+export default Menu;
